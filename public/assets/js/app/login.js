1be28a964e5b615b34e0c6a3f671f25588d52389
morello_cl <morello.cl@gmail.com>
2020-01-17 00:23:43 -0300

    init commit

---
+++ public/assets/js/app/login.js
@@ -0,0 +1,29 @@
+(function($) {
+    "use strict";
+
+    $("#btn-login").click(function(e) {
+        e.preventDefault();
+
+        $("#btn-login").attr( 'disabled', true );
+
+        const _login = {
+            username: 'pruebas',
+            password: 'Primus123'
+        }
+
+        axios.post( 'http://200.54.149.45/PrimusCapital.WebClienteApi/api/login/authenticate', _login, { 
+                timeout: 60000,
+                withCredentials: false
+            })
+            .then(r => {
+                console.log(r.data);
+            })
+            .catch(err => {
+                console.log(err.code);
+                console.log(err.message);
+                console.log(err.stack);
+            });
+
+        $("#btn-login").attr('disabled', false);
+	});
+})(jQuery);
