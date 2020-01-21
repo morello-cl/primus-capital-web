dc847048e56d19fbcc916855b44e90d8123097ae
morello_cl <morello.cl@gmail.com>
2020-01-21 12:21:16 -0300

    ot

--- public/assets/js/app/otorgamiento.js
+++ public/assets/js/app/otorgamiento.js
@@ -1,6 +1,52 @@
 (function($) {
     "use strict";
 
+    const date_now = moment().startOf("day");
+
+    const date_end = moment().startOf("day");
+
+    const date_ini = moment().add(-6, 'M');
+
+    console.log('date_ini', date_ini.format('DD-MM-YYYY'));
+    
+    
+    $("#ot_date_ini").datetimepicker({
+		format: "DD-MM-YYYY",
+		locale: "es",
+		defaultDate: date_ini
+    });
+    
+	$("#ot_date_end").datetimepicker({
+		format: "DD-MM-YYYY",
+		locale: "es",
+		defaultDate: date_end,
+		minDate: date_ini,
+        maxDate: date_end
+    });
+    
+    $("#ot_date_ini").datetimepicker().on("dp.change", function(e) {
+		$("#ot_date_end").datetimepicker({
+			minDate: $("#ot_date_ini").data("DateTimePicker").date()
+        });
+    });
+    $("#ot_date_end").datetimepicker().on("dp.change", function(e) {
+		$("#ot_date_ini").datetimepicker({
+			maxDate: $("#ot_date_end").data("DateTimePicker").date()
+        });
+    });
+
+    console.log('ot_op2', $('input:radio[name=ot_op2]:checked').val());
+    $('#ot_opt_op2').hide();
+
+    $('input:radio[name=ot_op2]').click(function(e){
+        if($('input:radio[name=ot_op2]:checked').val() === 'ot_opt_per') {
+            $('#ot_opt_op2').show();
+        } else {
+            $('#ot_opt_op2').hide();
+        }
+    });
+    
+
     $("#tbl_award").bootstrapTable({
 		columns: [
 			{
@@ -114,7 +160,7 @@
                 searchable: true,
 			}
 		],
-		url: '/award/api/sp_11_res/2019-04-01/2019-06-27',
+		url: `/award/api/sp_11_res/${'1900-01-01'}/${date_now.format('YYYY-MM-DD')}`,
 		locale: "es-SP",
 		sortName: "data.fecha",
 		sortOrder: "desc",
@@ -138,6 +184,14 @@
         e.preventDefault();
 
         console.log('#btn-ot-search', $('#reservation').val());
+
+        const dt_ini = $("#ot_date_ini").data("DateTimePicker").date().format("YYYY-MM-DD");
+        const dt_end = $("#ot_date_end").data("DateTimePicker").date().format("YYYY-MM-DD");
+        const __url = `/award/api/sp_11_res/${dt_ini}/${dt_end}`;
+
+        $("#tbl_award").bootstrapTable("refresh", {
+            url: __url,
+        });
 
         /*
         axios.get(`/award/api/sp_11_res/`, {
