6c04d54d9fc213cd86b50908b047270c24c9f1ae
morello_cl <morello.cl@gmail.com>
2020-02-10 12:31:09 -0300

    cartera vigente

---
+++ routes/wallet-staft_route.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const passport = require('passport');
+
+function isLoggedIn(req, res, next) {
+  console.log('isLoggedIn', req.user);
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated()) return next();
+
+	// if they aren't redirect them to the home page
+	return res.redirect("/login");
+}
+
+router.get('/sp_13_res', isLoggedIn, function(req, res, next){
+    let _rut = req.query.rut ? req.query.rut : 0;
+
+    console.log('res.rut', _rut);
+
+    axios.post( 'http://200.54.149.45/PrimusCapital.WebClienteApi/api/webcliente/sp_13_res', {
+        rut: _rut,
+        fdesde: req.query.date.gte,
+        fhasta: req.query.date.lte,
+        codempl : 0,
+        codcli: 0
+    }, {
+        headers: { Authorization: `Bearer ${req.user.access_token}` }
+    })
+        .then(function(r){
+            console.log(r.data);
+
+            res.json(r.data);
+        })
+        .catch(function(err){
+            console.log('err', err);
+
+            res.status(400).json({
+            });
+        });
+    
+});
+
+router.get('/sp_13_det', isLoggedIn, function(req, res, next){
+    let _rut = req.query.rut ? req.query.rut : 0;
+
+    console.log('det.rut', _rut);
+
+    axios.post( 'http://200.54.149.45/PrimusCapital.WebClienteApi/api/webcliente/sp_13_det', {
+        rut: _rut,
+        fdesde: req.query.date.gte,
+        fhasta: req.query.date.lte,
+        codempl : 0,
+        codcli: 0
+    }, {
+        headers: { Authorization: `Bearer ${req.user.access_token}` }
+    })
+        .then(function(r){
+            console.log(r.data);
+
+            res.json(r.data);
+        })
+        .catch(function(err){
+            console.log('err', err);
+
+            res.status(400).json({
+            });
+        });
+    
+});
+
+router.get('/sp_13_doc', isLoggedIn, function(req, res, next){
+    let _rut = req.query.rut ? req.query.rut : 0;
+
+    console.log('doc.rut', _rut);
+
+    axios.post( 'http://200.54.149.45/PrimusCapital.WebClienteApi/api/webcliente/sp_13_doc', {
+        rut: _rut,
+        fdesde: req.query.date.gte,
+        fhasta: req.query.date.lte,
+        codempl : 0,
+        codcli: 0
+    }, {
+        headers: { Authorization: `Bearer ${req.user.access_token}` }
+    })
+        .then(function(r){
+            console.log(r.data);
+
+            res.json(r.data);
+        })
+        .catch(function(err){
+            console.log('err', err);
+
+            res.status(400).json({
+            });
+        });
+    
+});
+
+module.exports = router;
