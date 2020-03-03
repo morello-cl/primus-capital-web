b02ef54f4cc18fa49228b6ecd0d6a053e996f6c5
morello_cl <morello.cl@gmail.com>
2020-03-03 13:32:10 -0300

    recibimos la info desde otorgamiento

--- public/assets/js/app/award.js
+++ public/assets/js/app/award.js
@@ -1076,7 +1076,11 @@ function urlSp11IndApl(rut, contrato) {
 								$('#ot_apli_cta').val(numeral(r.data[0].aplcxc).format("$ 0,000[.]0"));
 								$('#ot_apli_prote').val(numeral(r.data[0].aplprotesto).format("$ 0,000[.]0"));
 
-								$('#ot_apli_doc').attr('href', `/cancellations?page=abono&rut=${_awardCliente}&contrato=${_awardContrato}`);
+								if(r.data[0].aplicacionadocto > 0) {
+									$('#ot_apli_doc').attr('href', `/cancellations?page=abono&rut=${_awardCliente}&contrato=${_awardContrato}`);
+								} else {
+									$('#ot_apli_doc').attr('href', '');
+								}
 
 							} else {
 								$('#ot_apli_doc').val('');
